a600313b98ab0c81c50eb48cd2ac7b51acb3df0a
kiangtengl <kiangtengl@gmail.com>
2013-07-15 20:05:15 +0800

    Parity with commit yesterday, but without the need for System.Windows.Interactivity, telerik controls (not used by don't know why it was added).
    Also running on .NET 4.0, hopefully should solve compatibility issues.

--- WPF Gui/MainWindow.xaml.cs
+++ WPF Gui/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using WPF_GUI.ViewModel;
+using MahApps.Metro.Controls;
 
 namespace WPF_GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
